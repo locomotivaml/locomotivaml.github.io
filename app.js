59a290f67cba9973d11277fba5d49f9deb0c65b6
João Netto <hi@joaonetto.me>
2015-02-24 11:56:53 -0300

    Ideas

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use('/assets', express.static(__dirname + '/public'))
+app.use('/vendor', express.static(__dirname + '/bower_components'));
+
 app.get('/', function (req, res) {
     res.render('home');
 });
