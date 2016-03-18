0fa2ede0b4786c2b4049b75d46af565ee344a582
João Pedro Netto <hi@joaonetto.me>
2016-03-18 15:22:42 -0300

    new navigation bar and multi colored logo

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,17 @@
 $(function(){
   $('.player').YTPlayer();
+
+  $(window).on('activate.bs.scrollspy', function(e, data) {
+    console.log(data.relatedTarget);
+    if(data.relatedTarget == '#home' || data.relatedTarget == '#about' || data.relatedTarget == '#contact') {
+      $('#navbar-main').removeClass('navbar-light').addClass('navbar-dark');
+      $('#navbar-main .navbar-brand img.logo-white').removeClass('hidden-xs-up');
+      $('#navbar-main .navbar-brand img.logo-dark').addClass('hidden-xs-up');
+    }
+    else {
+      $('#navbar-main').removeClass('navbar-dark').addClass('navbar-light');
+      $('#navbar-main .navbar-brand img.logo-white').addClass('hidden-xs-up');
+      $('#navbar-main .navbar-brand img.logo-dark').removeClass('hidden-xs-up');
+    }
+  });
 });
